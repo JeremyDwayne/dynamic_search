d4ef2387da76517279e81b53f56cb742a9c138ec
Jeremy Winterberg <winterjd@uwec.edu>
2017-11-19 14:22:21 -0600

    new version of search method

--- lib/dynamic_search.rb
+++ lib/dynamic_search.rb
@@ -1,23 +1,36 @@
 module DynamicSearch
   extend ActiveSupport::Concern
-	# scope :status, -> (status) { where status: status }
+  # scope :status, -> (status) { where status: status }
 
-	module ClassMethods
-		def search(search)
-			if search && !search.empty?
-				# sanitize_sql is used over sanitize because sanitize removes html
-				# tags but because we are doing sql calls we need to sanitize the
-				# string for that.
-				results = self.all
-				for part in search.split(/\s+/)
-					part = "#{sanitize_sql(part)}%"
-					word = "% #{sanitize_sql(part)}%"
-                    results = results.search_params(part).or(results.search_params(word))
-				end
-				return results.uniq
-			else
-				self.all
-			end
-		end
-	end
+  module ClassMethods
+
+    def search(search, columns = nil)
+      if search.present?
+        # sanitize_sql is used over sanitize because sanitize removes html
+        # tags but because we are doing sql calls we need to sanitize the
+        # string for that. I have not included an escape charecter so this
+        # might limit search but is safer.
+        columns ||= self.column_names.reject{|e| e.end_with?("_id")} - ["id", "created_at", "updated_at"]
+        i = 0
+        params = Hash.new
+        sql = Array.new
+        for s in search.split
+          i += 1
+          params["first#{i}".to_sym] = "#{sanitize_sql(s)}%"
+          params["nth#{i}".to_sym] = "% #{sanitize_sql(s)}%"
+          # Search everything except the id columns
+          statements = Array.new
+          for column in columns
+            statements << "#{column} LIKE :first#{i}"
+            statements << "#{column} LIKE :nth#{i}"
+          end
+          sql << "(#{statements.join(" OR ")})"
+        end
+        self.where(sql.join(" AND "), params)
+      else
+        self.all
+      end
+    end
+
+  end
 end
